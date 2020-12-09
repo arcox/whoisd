919651c454f3b1999aa527294a7687f55bd8048b
arcox <10953229+arcox@users.noreply.github.com>
2020-12-09 13:25:43 -0500

    Update server code to use new config files + improve code styling

--- whoisd.py
+++ whoisd.py
@@ -1,24 +1,27 @@
+# stdlib imports
 import json
 import logging
 import logging.handlers
 import socket
 import socketserver
 
-CONFIG_PATH = 'config.json'
+CONFIG_PATH = 'server.conf'
+DATA_PATH = 'data.json'
 LOG_PATH = 'log.txt'
 
 class WhoisRequestHandler(socketserver.StreamRequestHandler):
     def handle(self):
         logger = logging.getLogger('whoisd')
-        config = self.server.whois_config
-        hits = []
+
         request = self.rfile.readline()
         request = request.decode().strip().lower()
 
         # process request
         logger.info("Processing request '{}' from {}...".format(
             request, self.client_address[0]))
-        for entry in self.server.whois_config['entries']:
+
+        hits = []
+        for entry in self.server.whois_data['entries']:
             for k, v in entry.items():
                 if k.startswith('!'):
                     if request in v.lower():
@@ -28,16 +31,17 @@ class WhoisRequestHandler(socketserver.StreamRequestHandler):
         # prepare response
         response = "No matches found.\r\n"
         if hits:
-            response = self.server.whois_config['header'] + "\r\n"
+            response = self.server.whois_data['header'] + "\r\n"
             counter = 0
 
             for hit in hits:
                 counter += 1
-                response += "\r\n-- Record {} of {} --\r\n".format(counter, len(hits))
+                response += "\r\n-- Record {} of {} --\r\n".format(
+                    counter, len(hits))
                 for k, v in hit.items():
                     k = k[1:] if k.startswith('!') else k
-                    response += k + ": " + v + "\r\n"
-            response += "\r\n" + self.server.whois_config['footer'] + "\r\n"
+                    response += "{}: {}\r\n".format(k, v)
+            response += "\r\n{}\r\n".format(self.server.whois_data['footer'])
 
         logger.info("Response:\n{}".format(response))
         logger.info("--------------------------------------")
@@ -52,24 +56,42 @@ def main():
             LOG_PATH, maxBytes=50*1024*1024, backupCount=5))
 
     # read config
+    config = {}
     with open(CONFIG_PATH, 'rt') as conf:
-        data = json.load(conf)
-        if data:
-            if 'header' not in data:
+        content = json.load(conf)
+        if content:
+            if ('listen_ip' not in content or 
+                not isinstance(content['listen_ip'], str)):
+                raise Exception("Expected 'listen_ip' string.")
+            if ('listen_port' not in content or 
+                not isinstance(content['listen_port'], int)):
+                raise Exception("Expected 'listen_port' integer.")
+            config = content
+
+    # read database
+    data = None
+    with open(DATA_PATH, 'rt') as conf:
+        content = json.load(conf)
+        if content:
+            if ('header' not in content or 
+                not isinstance(content['header'], str)):
                 raise Exception("Expected 'header' string.")
-            if 'footer' not in data:
+            if ('footer' not in content or 
+                not isinstance(content['footer'], str)):
                 raise Exception("Expected 'footer' string.")
-            if 'entries' not in data or not isinstance(data['entries'], list):
+            if ('entries' not in content or 
+                not isinstance(content['entries'], list)):
                 raise Exception("Expected 'entries' list.")
-            config = data
+            data = content
 
     # start server
     logger.info("***************************************")
     logger.info("************* STARTING UP *************")
     logger.info("***************************************")
     srv = socketserver.ThreadingTCPServer(
-        ('0.0.0.0', 43), WhoisRequestHandler, False)
-    srv.whois_config = config
+        (config['listen_ip'], config['listen_port']), WhoisRequestHandler,
+        False)
+    srv.whois_data = data
     srv.allow_reuse_address = True
     srv.server_bind()
     srv.server_activate()
